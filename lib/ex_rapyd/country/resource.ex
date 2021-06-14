8229232d375e94f5965682a2112cbed10490d7e9
Robert Chu <RobChooses@users.noreply.github.com>
2021-06-14 06:25:07 +0100

    Formatting

--- lib/ex_rapyd/country/resource.ex
+++ lib/ex_rapyd/country/resource.ex
@@ -1,20 +1,20 @@
 defmodule ExRaypd.Resource.Country do
-    @moduledoc """
-    Elixir wrapper for the Rapyd API - Resource Methods - Country API
-    """
-  
-    @doc """
-    List countries
+  @moduledoc """
+  Elixir wrapper for the Rapyd API - Resource Methods - Country API
+  """
 
-    ## Example
+  @doc """
+  List countries
 
-      iex> ExRapyd.Resource.Country.List()
-      {:ok, response}
+  ## Example
 
-    """
-    def list() do
-      path = "/data/countries"
-      client = ExRapyd.client(%{http_method: "get", path: path, body: ""})
-      Tesla.get(client, path)
-    end
+    iex> ExRapyd.Resource.Country.List()
+    {:ok, response}
+
+  """
+  def list() do
+    path = "/data/countries"
+    client = ExRapyd.client(%{http_method: "get", path: path, body: ""})
+    Tesla.get(client, path)
+  end
 end
