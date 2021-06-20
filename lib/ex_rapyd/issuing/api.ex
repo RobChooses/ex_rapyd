efb1f071ab886e82a70e00b37db9ea7d4d2b9c39
Robert Chu <RobChooses@users.noreply.github.com>
2021-06-20 08:00:21 +0100

    Add gt bank account and simulate bank transfer

--- lib/ex_rapyd/issuing/api.ex
+++ lib/ex_rapyd/issuing/api.ex
@@ -21,4 +21,35 @@ defmodule ExRapyd.Issuing.API do
     client = ExRapyd.client(%{http_method: "post", path: path, body: body}, options)
     Tesla.post(client, path, issue_body)
   end
+
+  @doc """
+  Get Bank Account History of Bank Account Number
+  ## Example
+
+    iex> ExRapyd.Issuing.API.get_bank_account_history("issuing_xxxx")
+    {:ok, response}
+
+  """
+  def get_bank_account_history(bank_account_id, options \\ []) when is_binary(bank_account_id) do
+    path = "/issuing/bankaccounts/#{bank_account_id}"
+    client = ExRapyd.client(%{http_method: "get", path: path, body: ""}, options)
+    Tesla.get(client, path)
+  end
+
+  @doc """
+  Simulate a Bank Transfer to Wallet (only in sandbox)
+
+  ## Example
+
+    iex> issue_transfer_body = %{amount: "1000.00", currency: "GBP", issued_bank_account: "issuing_xxxx"}
+    iex> ExRapyd.Issuing.API.simulate_bank_transfer(issue_transfer_body)
+    {:ok, response}
+
+  """
+  def simulate_bank_transfer(issue_transfer_body, options \\ []) do
+    path = "/issuing/bankaccounts/bankaccounttransfertobankaccount"
+    body = Jason.encode!(issue_transfer_body)
+    client = ExRapyd.client(%{http_method: "post", path: path, body: body}, options)
+    Tesla.post(client, path, issue_transfer_body)
+  end
 end
